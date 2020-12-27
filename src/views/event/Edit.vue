02600033af775f522868422fb1777475ce6144c9
LoralieFlint <loralie.flint0128@gmail.com>
2020-12-27 13:38:06 -0800

    completed

---
+++ src/views/event/Edit.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>edit the event here</p>
+</template>
+
+<script>
+  export default {
+    props: ["event"]
+  }
+</script>
---
+++ src/views/event/Register.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>register for the event here</p>
+</template>
+
+<script>
+  export default {
+    props: ["event"]
+  }
+</script>
