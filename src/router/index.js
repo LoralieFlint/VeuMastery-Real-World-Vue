a896f244e543cea9ebb935104fc5945029398a88
LoralieFlint <loralie.flint0128@gmail.com>
2020-12-26 19:40:03 -0800

    touring vue router lesson 3

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import About from "../views/About.vue";
 import EventDetails from "../views/EventDetails.vue";
+import { parseInt } from "core-js/fn/number";
 
 
 const routes = [
@@ -9,6 +10,9 @@ const routes = [
     path: "/",
     name: "EventList",
     component: EventList,
+    props: route => ({
+      page: parseInt(route.query.page) || 1
+    }) 
   },
   {
     path: "/about",
